62477622f7d16e8ae0eabcb14f1d6966a6f01fdb
Euston Lee <lee.yousheng@gmail.com>
2020-09-21 09:02:59 +0800

    Allow customer to have transaction session

--- pkg/app/customer.go
+++ pkg/app/customer.go
@@ -4,9 +4,75 @@ import (
 	"errors"
 )
 
+// DepositSession allows customer to perform a transaction
+type DepositSession struct {
+	depositPlans []DepositPlan
+	deposits     []float32
+}
+
 // Customer is the portfolio owner
 type Customer struct {
-	portfolios []*Portfolio
+	ID             string
+	portfolios     []*Portfolio
+	DepositSession *DepositSession
+}
+
+// NewCustomer instantiate a new customer with no portfolios
+func NewCustomer(id string) Customer {
+	return Customer{ID: id, portfolios: []*Portfolio{}}
+}
+
+// AddPortfolio adds portfolio after determining validity
+func (c *Customer) AddPortfolio(name string) error {
+	for _, p := range c.portfolios {
+		if p.Name == name {
+			return errors.New("portfolio with specfied name already added")
+		}
+	}
+
+	newP, err := NewPortfolio(name)
+	if err != nil {
+		return err
+	}
+	c.portfolios = append(c.portfolios, &newP)
+	return nil
+}
+
+// StartSession starts a deposit session
+func (c *Customer) StartSession() error {
+	if c.DepositSession != nil {
+		return errors.New("another transaction is still active")
+	}
+	c.DepositSession = &DepositSession{depositPlans: []DepositPlan{}, deposits: []float32{}}
+	return nil
+}
+
+// Deposit represents the amount the customer has deposit
+func (c *Customer) Deposit(amount float32) error {
+	if c.DepositSession == nil {
+		return errors.New("no active session")
+	}
+	if amount < 0 {
+		return errors.New("amount is negative")
+	}
+	c.DepositSession.deposits = append(c.DepositSession.deposits, amount)
+	return nil
+}
+
+// PayDepositPlan represnts the plans the customer would like to pay
+func (c *Customer) PayDepositPlan(plan DepositPlan) error {
+	if c.DepositSession == nil {
+		return errors.New("no active session")
+	}
+
+	for _, dp := range c.DepositSession.depositPlans {
+		if dp.Name() == plan.Name() {
+			return errors.New("duplicate plan name in session")
+		}
+	}
+
+	c.DepositSession.depositPlans = append(c.DepositSession.depositPlans, plan)
+	return nil
 }
 
 // PerformDeposit split the passed in deposit into the respective portfolio
--- pkg/app/customer_test.go
+++ pkg/app/customer_test.go
@@ -6,6 +6,154 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNewCustomer_shouldReturnCustomer(t *testing.T) {
+	c := NewCustomer("test")
+	assert.Equal(t, "test", c.ID)
+	assert.Empty(t, c.portfolios)
+}
+
+func TestAddPortfolio_shouldAddPortfolioToCustomer(t *testing.T) {
+	testAddPortfolio := func(portfolios []*Portfolio, portfolioName string, expected []*Portfolio) {
+		c := Customer{ID: "name", portfolios: portfolios}
+		err := c.AddPortfolio(portfolioName)
+		assert.Nil(t, err)
+		assert.Equal(t, expected, c.portfolios)
+	}
+
+	testAddPortfolio([]*Portfolio{}, "p1", []*Portfolio{{Name: "p1"}})
+	testAddPortfolio([]*Portfolio{{Name: "p1"}, {Name: "p2"}}, "p3", []*Portfolio{{Name: "p1"}, {Name: "p2"}, {Name: "p3"}})
+}
+
+func TestAddPortfolio_shouldReturnError_givenDuplicatePortfolioName(t *testing.T) {
+	testAddPortfolio := func(portfolios []*Portfolio, portfolioName string, expected []*Portfolio) {
+		c := Customer{ID: "name", portfolios: portfolios}
+		err := c.AddPortfolio(portfolioName)
+		assert.NotNil(t, err)
+		assert.Equal(t, "portfolio with specfied name already added", err.Error())
+		assert.Equal(t, expected, c.portfolios)
+	}
+
+	testAddPortfolio([]*Portfolio{{Name: "p1"}, {Name: "p2"}}, "p1", []*Portfolio{{Name: "p1"}, {Name: "p2"}})
+}
+
+func TestAddPortfolio_shouldReturnError_givenPortfolioNameMissing(t *testing.T) {
+	testAddPortfolio := func(portfolios []*Portfolio, portfolioName string, expected []*Portfolio) {
+		c := Customer{ID: "name", portfolios: portfolios}
+		err := c.AddPortfolio(portfolioName)
+		assert.NotNil(t, err)
+		assert.Equal(t, "invalid name", err.Error())
+		assert.Equal(t, expected, c.portfolios)
+	}
+
+	testAddPortfolio([]*Portfolio{}, "", []*Portfolio{})
+}
+
+func TestStartSession_shouldStartANewSession(t *testing.T) {
+	testStartSession := func() {
+		c := Customer{}
+		err := c.StartSession()
+		assert.Nil(t, err)
+		assert.NotNil(t, c.DepositSession)
+		assert.Equal(t, &DepositSession{depositPlans: []DepositPlan{}, deposits: []float32{}}, c.DepositSession)
+	}
+
+	testStartSession()
+}
+
+func TestStartSession_shouldReturnError_givenAnotherSessionIsActive(t *testing.T) {
+	testStartSession := func() {
+		c := Customer{DepositSession: &DepositSession{}}
+		err := c.StartSession()
+		assert.NotNil(t, err)
+		assert.Equal(t, "another transaction is still active", err.Error())
+	}
+
+	testStartSession()
+}
+
+func TestPayDepositPlan_shouldUpdatePlansInSession(t *testing.T) {
+	testPayDepositPlan := func(initialPlans []DepositPlan, plan DepositPlan, expected []DepositPlan) {
+		c := Customer{DepositSession: &DepositSession{depositPlans: initialPlans, deposits: []float32{}}}
+		err := c.PayDepositPlan(plan)
+		assert.Nil(t, err)
+		assert.Equal(t, expected, c.DepositSession.depositPlans)
+	}
+
+	testPayDepositPlan(
+		[]DepositPlan{},
+		&baseDepositPlan{name: "test", planType: "monthly", portfolioRatio: map[string]float32{"Retirement": 100}},
+		[]DepositPlan{&baseDepositPlan{name: "test", planType: "monthly", portfolioRatio: map[string]float32{"Retirement": 100}}},
+	)
+	testPayDepositPlan(
+		[]DepositPlan{&baseDepositPlan{name: "test1", planType: "monthly", portfolioRatio: map[string]float32{"Retirement": 100}}},
+		&baseDepositPlan{name: "test2", planType: "one-time", portfolioRatio: map[string]float32{"Retirement": 100}},
+		[]DepositPlan{&baseDepositPlan{name: "test1", planType: "monthly", portfolioRatio: map[string]float32{"Retirement": 100}}, &baseDepositPlan{name: "test2", planType: "one-time", portfolioRatio: map[string]float32{"Retirement": 100}}},
+	)
+}
+
+func TestPayDepositPlan_shouldReturnError_givenNoActiveSession(t *testing.T) {
+	testPayDepositPlan := func(plan DepositPlan) {
+		c := Customer{DepositSession: nil}
+		err := c.PayDepositPlan(plan)
+		assert.Error(t, err)
+		assert.Equal(t, "no active session", err.Error())
+	}
+
+	testPayDepositPlan(&baseDepositPlan{name: "test", planType: "monthly", portfolioRatio: map[string]float32{"Retirement": 100}})
+}
+
+func TestPayDepositPlan_shouldReturnError_givenDuplicateDepositPlanName(t *testing.T) {
+	testPayDepositPlan := func(initialPlans []DepositPlan, plan DepositPlan, expected []DepositPlan) {
+		c := Customer{DepositSession: &DepositSession{depositPlans: initialPlans, deposits: []float32{}}}
+		err := c.PayDepositPlan(plan)
+		assert.Error(t, err)
+		assert.Equal(t, "duplicate plan name in session", err.Error())
+		assert.Equal(t, expected, c.DepositSession.depositPlans)
+	}
+
+	testPayDepositPlan(
+		[]DepositPlan{&baseDepositPlan{name: "test", planType: "monthly", portfolioRatio: map[string]float32{"Retirement": 100}}},
+		&baseDepositPlan{name: "test", planType: "monthly", portfolioRatio: map[string]float32{"Retirement": 100}},
+		[]DepositPlan{&baseDepositPlan{name: "test", planType: "monthly", portfolioRatio: map[string]float32{"Retirement": 100}}},
+	)
+	testPayDepositPlan(
+		[]DepositPlan{&baseDepositPlan{name: "test1", planType: "monthly", portfolioRatio: map[string]float32{"Retirement": 100}}, &baseDepositPlan{name: "test2", planType: "one-time", portfolioRatio: map[string]float32{"Retirement": 100}}},
+		&baseDepositPlan{name: "test2", planType: "one-time", portfolioRatio: map[string]float32{"Retirement": 100}},
+		[]DepositPlan{&baseDepositPlan{name: "test1", planType: "monthly", portfolioRatio: map[string]float32{"Retirement": 100}}, &baseDepositPlan{name: "test2", planType: "one-time", portfolioRatio: map[string]float32{"Retirement": 100}}},
+	)
+}
+
+func TestDeposit_shouldDepositAmount(t *testing.T) {
+	testDeposit := func(initialDeposits []float32, amount float32, expected []float32) {
+		c := Customer{DepositSession: &DepositSession{depositPlans: []DepositPlan{}, deposits: initialDeposits}}
+		err := c.Deposit(amount)
+		assert.Nil(t, err)
+		assert.Equal(t, expected, c.DepositSession.deposits)
+	}
+	testDeposit([]float32{}, 10.10, []float32{10.1})
+	testDeposit([]float32{20}, 0, []float32{20, 0})
+}
+
+func TestDeposit_shouldReturnError_givenNoActiveSession(t *testing.T) {
+	testDeposit := func(amount float32) {
+		c := Customer{DepositSession: nil}
+		err := c.Deposit(amount)
+		assert.Error(t, err)
+		assert.Equal(t, "no active session", err.Error())
+	}
+	testDeposit(10.10)
+}
+
+func TestDeposit_shouldReturnError_givenAmountIsNegative(t *testing.T) {
+	testDeposit := func(initialDeposits []float32, amount float32, expected []float32) {
+		c := Customer{DepositSession: &DepositSession{depositPlans: []DepositPlan{}, deposits: initialDeposits}}
+		err := c.Deposit(amount)
+		assert.Error(t, err)
+		assert.Equal(t, "amount is negative", err.Error())
+	}
+	testDeposit([]float32{20}, -1, []float32{20})
+}
+
 func TestPerformDeposit_shouldUpdatePortfolioBalance(t *testing.T) {
 	testPerformDeposit := func(portfolios []*Portfolio, depositPlans []DepositPlan, deposits []float32, updatedPortfolios []*Portfolio) {
 		c := Customer{portfolios: portfolios}
